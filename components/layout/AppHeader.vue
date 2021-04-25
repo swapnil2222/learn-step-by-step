e869140ed28ecefda912ebf27f49dbb125915deb
Swapnil <swapnil.renge@geospoc.com>
2021-04-25 13:55:54 +0530

    feat: add navbar

--- components/layout/AppHeader.vue
+++ components/layout/AppHeader.vue
@@ -1,13 +1,18 @@
 <template>
   <nuxt-link :to="'/'">
     <header class="w-full py-2 bg-purple-500">
-      <div class="container px-4 mx-auto">
-        <div class="flex">
-          <span class="text-2xl font-semibold text-white">Swapnil's Blog</span>
+      <div class="container flex items-center px-4 mx-auto">
+        <div class="flex flex-1">
+          <span class="text-2xl font-semibold text-white"
+            >Learn Step by Step</span
+          >
           <UiSearchBox
             class="flex-1 max-w-sm ml-12"
             :search="fetchSuggestions"
           />
+        </div>
+        <div class="flex">
+          <UiNavBar />
         </div>
       </div>
     </header>
---
+++ components/ui/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex font-bold text-white">
+    <nuxt-link class="mx-2" :to="`/`">Articles</nuxt-link>
+    <nuxt-link class="mx-2" :to="`/`">About</nuxt-link>
+    <nuxt-link class="mx-2" :to="`/`">Contact Us</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
